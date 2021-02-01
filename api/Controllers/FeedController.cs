0fa334725138cf61719c57afb81e292851a7ef05
Prashant Khandelwal <prashant@midnightprogrammer.net>
2021-02-01 21:05:56 +0530

    Initial commit for the api.

---
+++ api/Controllers/FeedController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FeedController : Controller
+    {
+        [HttpGet]
+        public ObjectResult Get()
+        {
+            return Ok(new { Value = "This is a return message" });
+        }
+    }
+}
